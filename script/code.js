1fe8eb55a33b035ea98751e39c96e614e71a042f
SmasherK <ksikalan@gmail.com>
2023-05-17 09:03:43 +0200

    done

---
+++ script/code.js
@@ -0,0 +1,16 @@
+let fname ="Andrew";
+console.log(typeof fname);
+fname=209
+console.log(typeof fname);
+let isTrue =false;
+console.log(typeof isTrue);
+let exData;
+console.log(typeof exData);
+let emTy =null;
+console.log(typeof emTy);
+let delData ={
+    nm :"bobo"
+}
+console.log(typeof delData);
+let subs =[124,"njee"]
+console.log(typeof subs);
